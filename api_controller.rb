d050521a8415d8f0a0a2f01c0aa64a6a54004f65
thesilentcodingguy <thebrokecollegeguy@gmail.com>
2026-04-06 13:03:55 +0530

    Update api_controller.rb

--- api_controller.rb
+++ api_controller.rb
@@ -62,3 +62,12 @@ class BooksController < ApplicationController
     params.permit(:title, :author, :price, :published_date)
   end
 end
+
+
+#MODEL
+class Book < ApplicationRecord
+  validates :title, presence: true
+  validates :author, presence: true
+  validates :price, presence: true, numericality: { greater_than: 0 }
+  validates :published_date, presence: true
+end
