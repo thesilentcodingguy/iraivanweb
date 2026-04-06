7ddc059b17396554ad243301abfeceb71a75bda5
thesilentcodingguy <thebrokecollegeguy@gmail.com>
2026-04-06 13:03:01 +0530

    Create api_controller.rb

---
+++ api_controller.rb
@@ -0,0 +1,64 @@
+class BooksController < ApplicationController
+  # GET /books
+  def index
+    @books = Book.all
+    render json: @books
+  end
+
+  # GET /books/:id
+  def show
+    @book = Book.find_by(id: params[:id])
+    
+    if @book
+      render json: @book
+    else
+      render json: { error: 'Book not found' }, status: :not_found
+    end
+  end
+
+  # POST /books
+  def create
+    @book = Book.new(book_params)
+    
+    if @book.save
+      render json: @book, status: :created
+    else
+      render json: { errors: @book.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  # PUT/PATCH /books/:id
+  def update
+    @book = Book.find_by(id: params[:id])
+    
+    if @book.nil?
+      render json: { error: 'Book not found' }, status: :not_found
+      return
+    end
+    
+    if @book.update(book_params)
+      render json: @book
+    else
+      render json: { errors: @book.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /books/:id
+  def destroy
+    @book = Book.find_by(id: params[:id])
+    
+    if @book.nil?
+      render json: { error: 'Book not found' }, status: :not_found
+      return
+    end
+    
+    @book.destroy
+    render json: { message: 'Book successfully deleted' }, status: :ok
+  end
+
+  private
+
+  def book_params
+    params.permit(:title, :author, :price, :published_date)
+  end
+end
